9a5fc0424ce98e69ec4cc95c398fa577f1727208
Shawlycode <kutinaorlando@gmail.com>
2021-09-13 22:40:50 +0000

    setup config component

---
+++ src/Firebase/Config.jsx
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyAtjPGk4nvn1ER6C8dTdW5SCAkrrkcgm1I",
+    authDomain: "user-app-b0bcd.firebaseapp.com",
+    projectId: "user-app-b0bcd",
+    storageBucket: "user-app-b0bcd.appspot.com",
+    messagingSenderId: "471917738484",
+    appId: "1:471917738484:web:f954ae6e8dc6f60156a1a1"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
