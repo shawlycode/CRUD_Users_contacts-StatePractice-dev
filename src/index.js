1b5be27fcfd93fe4880d83fe2b49b446d000a91f
Shawlycode <kutinaorlando@gmail.com>
2021-09-13 22:44:51 +0000

    imported store and provider

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import store from './Store/Store';
+import {Provider} from 'react-redux';
+
+ReactDOM.render(
+  
+    <Provider store ={store}>
+    <App />
+    </Provider>,
+  document.getElementById('root')
+);
+
+
